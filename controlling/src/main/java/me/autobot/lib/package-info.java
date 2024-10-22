2dc166baa992e5b86378c14cf4280d487087276b
jaidenagrimminck <jaidng1@gmail.com>
2024-10-23 01:34:44 +0200

    adding package infos!

---
+++ controlling/src/main/java/me/autobot/lib/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the library classes for the controlling module. Generally helper classes and utilities, or
+ * instantiating classes.
+ * */
+package me.autobot.lib;
---
+++ controlling/src/main/java/me/autobot/server/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package for server-side classes, such as the REST API or the WebSocket server.
+ */
+package me.autobot.server;
